7bfc5c33595de7e14a406ee1119b96f13e06660b
= <162285049+parrTh11@users.noreply.github.com>
2024-05-29 11:45:39 +0530

    End of Serction-4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,19 +1,21 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")] // api/users
-    public class UsersController : ControllerBase
+     // api/users
+     [Authorize]
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context){
             _context = context;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
         var users = await _context.Users.ToListAsync();
